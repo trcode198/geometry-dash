a3d76edc16ec0cf460a5e21caf716acbc9984878
trcode198 <tj.roberts198@gmail.com>
2025-03-05 15:10:22 +0000

    fk u

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,30 +1,42 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-    private ShapeRenderer shapeRenderer;
+    private ShapeRenderer sr;
+    private Player player;
 
     @Override
     public void create() {
-        shapeRenderer = new ShapeRenderer();
+        sr = new ShapeRenderer();
+        player = new Player(140, 200, 44);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0.01f, 0f, 0.5f, 1f);
 
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(1, 1, 1, 1);
-        shapeRenderer.rect(140, 210, 50, 50);
-        shapeRenderer.end();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            player.jump();
+        }
+
+        player.update();
+
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        sr.setColor(255, 140, 0, 255);
+        sr.rect(player.getX(), player.getY(), player.getSize(), player.getSize());
+        sr.setColor(Color.valueOf("1a0355"));
+        sr.rect(0,0,1980,200);
+        sr.end();
     }
 
     @Override
     public void dispose() {
-        shapeRenderer.dispose();
+        sr.dispose();
     }
 }
--- core/src/main/java/io/github/some_example_name/Player.java
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -3,11 +3,19 @@ package io.github.some_example_name;
 public class Player {
     private int x, y;
     private int size;
+    private double velocityY;
+    private boolean isJumping;
+    private double gravity = -0.876;
+    private final int jumpStrength = 15;
+    private final int groundLevel;
 
     public Player(int x, int y, int size) {
         this.x = x;
         this.y = y;
         this.size = size;
+        this.groundLevel = y;
+        this.velocityY = 0;
+        this.isJumping = false;
     }
 
     public int getX() {
@@ -32,5 +40,24 @@ public class Player {
 
     public void setSize(int size) {
         this.size = size;
+    }
+
+    public void jump() {
+        if (!isJumping) {
+            isJumping = true;
+            velocityY = jumpStrength;
+        }
+    }
+
+    public void update() {
+        if (isJumping) {
+            y += velocityY;
+            velocityY += gravity;
+            if (y <= groundLevel) {
+                y = groundLevel;
+                isJumping = false;
+                velocityY = 0;
+            }
+        }
     }
 }
